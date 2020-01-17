8acb74acbd7855e3ad44ea833665118f5815c5a5
Andrea Bijlsma <andrea_bijlsma@me.com>
2020-01-17 14:43:08 +0100

    Add additional info to first task

--- bikeshare.py
+++ bikeshare.py
@@ -22,7 +22,7 @@ def get_filters():
     print('Hello! Let\'s explore some US bikeshare data!')
 
 
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # TO DO: get user input for city (chicago, new york city, washington). Add a break into the code by using while loop to handle invalid inputs
 
     while True:
         city = input(' Would you like to see data from Chicago, New York City or Washington? \n').lower()
